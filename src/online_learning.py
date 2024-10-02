210d59e855916f7abef11611b79f90e9eeb7c271
HaoMAFRLu <hao.ma@tuebingen.mpg.de>
2024-10-02 16:02:53 +0200

    shift

--- src/online_learning.py
+++ src/online_learning.py
@@ -424,10 +424,10 @@ class OnlineLearning():
         yref_marker, path_marker = self.marker_initialization()
 
         model_idx = 0
-        model_switch_idx = [1, 2, 3, 4, 5]
-        # model_switch_idx = [1000, 2000, 3000, 4000, 5000, 
-        #                     6000, 7000, 8000, 9000, 10000, 
-        #                     11000, 12000, 13000, 14000]
+        # model_switch_idx = [1, 2, 3, 4, 5]
+        model_switch_idx = [1000, 2000, 3000, 4000, 5000, 
+                            6000, 7000, 8000, 9000, 10000, 
+                            11000, 12000, 13000, 14000]
 
         for i in range(nr_iterations):
             tt = time.time()
